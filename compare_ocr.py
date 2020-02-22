a179256f60dc4f557fdab05ef6f17e3d5a23ddfa
= <=>
2020-02-22 15:58:24 +1000

    Error handler decorator added. Code refactored. Tests extended. Test coverage: 100%

--- compare_ocr.py
+++ compare_ocr.py
@@ -12,61 +12,84 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import itertools
+import functools
+
+
+def catch_exception(f):
+    @functools.wraps(f)
+    def func(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            print('Caught exception in', f.__name__)
+            print(str(e))
+    return func
 
 
 class OCR_Analyzer:
 
-    def __init__(self, ocr_path_1, ocr_path_2, ocr_name_1, ocr_name_2, ocr_image_directory):
+    @catch_exception
+    def __init__(self, ocr_path_1, ocr_path_2, ocr_name_1, ocr_name_2):
         with open(ocr_path_1) as ocr_file_1:
             self.ocr_1 = pd.DataFrame(json.load(ocr_file_1))
         with open(ocr_path_2) as ocr_file_2:
             self.ocr_2 = pd.DataFrame(json.load(ocr_file_2))
+        self.ocr_1_plot = self.ocr_1.copy()
+        self.ocr_2_plot = self.ocr_2.copy()
         self.ocr_name_1 = ocr_name_1.lower()
         self.ocr_name_2 = ocr_name_2.lower()
-        self.images = [plt.imread(image) for image in glob.glob(ocr_image_directory + '/*.jpg')]
         self.image_scale = 100
         self.scale_trail = []
 
 
+    @catch_exception
+    def load_images(self, directory):
+        self.images = [plt.imread(image) for image in glob.glob(directory + '/*.jpg')]
+        if len(self.images) == 0:
+            raise IOError('No images found.')
+
+
+    @catch_exception
     def scale_bounds(self, scale=1.0):
-        self.ocr_1['bounds'] = self.ocr_1['bounds'].apply(lambda l: [i*scale for i in l])
-        self.ocr_2['bounds'] = self.ocr_2['bounds'].apply(lambda l: [i*scale for i in l])
-        if scale != 1.0:
-            self.scale_trail.append(scale)
+        if scale == 0:
+            raise ValueError('Scale value can not be zero.')
+        scale_func = lambda l: [round(i*scale) for i in l]
+        self.ocr_1_plot['bounds'] = self.ocr_1_plot['bounds'].apply(scale_func)
+        self.ocr_2_plot['bounds'] = self.ocr_2_plot['bounds'].apply(scale_func)
 
 
+    @catch_exception
     def reverse_scaling(self):
-        for scale in reversed(self.scale_trail):
-            self.ocr_1['bounds'] = self.ocr_1['bounds'].apply(lambda l: [round(i/scale) for i in l])
-            self.ocr_2['bounds'] = self.ocr_2['bounds'].apply(lambda l: [round(i/scale) for i in l])
-            self.scale_trail.remove(scale)
+        self.ocr_1_plot['bounds'] = self.ocr_1['bounds']
+        self.ocr_2_plot['bounds'] = self.ocr_2['bounds']
 
 
+    @catch_exception
     def show_page(self, page):
-        try:
-            im_data = self.images[page-1]
-        except IndexError:
-            print('Page does not exist.')
-            return 0
+        if page > len(self.images):
+            raise IndexError('Page does not exist.')
+        im_data = self.images[page-1]
         ax = plot_page(im_data, self.image_scale)
         plt.show()
         return ax
 
 
-    def show_boundary_boxes(self, page, word, table='both', fuzz_threshold=100):
-        try:
-            im_data = self.images[page-1]
-        except IndexError:
-            print('Page does not exist.')
-            return 0
-        tables = [self.ocr_1, self.ocr_2]
-        if table == 'both':
+    @catch_exception
+    def show_boundary_boxes(self, page, word, engine='both', fuzz_threshold=100):
+        if page > len(self.images):
+            raise IndexError('Page does not exist.')
+        im_data = self.images[page-1]
+        tables = [self.ocr_1_plot, self.ocr_2_plot]
+        t_names = [self.ocr_name_1, self.ocr_name_2]
+        if engine == 'both':
             page_data_1 = extract_page(tables[0], page)
             page_data_2 = extract_page(tables[1], page)
-            red_boxes = find_boundaries(page_data_1, word, fuzz_threshold)
+            red_boxes = find_boundaries(page_data_1,word, fuzz_threshold)
             green_boxes = find_boundaries(page_data_2, word, fuzz_threshold)
         else:
-            table = tables[table-1]
+            if engine not in t_names:
+                raise ValueError('OCR data not detected.')
+            table = tables[t_names.index(engine)]
             page_data = extract_page(table, page)
             red_boxes = find_boundaries(page_data, word, fuzz_threshold)
             green_boxes = []
@@ -75,14 +98,22 @@ class OCR_Analyzer:
 
         ax = plot_page(im_data, self.image_scale)
         ax = plot_boundary_boxes(ax, red_boxes, green_boxes)
-        ax = build_legend(ax, self.ocr_name_1, self.ocr_name_2, red_count, green_count)
+        ax = build_legend(ax,
+                          self.ocr_name_1,
+                          self.ocr_name_2,
+                          red_count,
+                          green_count)
 
         plt.show()
 
         return ax
 
 
+    @catch_exception
     def compare_ocr_outputs(self, iou_threshold=0.4, verbose=0, indent=4):
+
+        if verbose not in [0, 1]:
+            raise ValueError('Verbose level must be 0 or 1.')
 
         output = []
 
@@ -102,7 +133,8 @@ class OCR_Analyzer:
                     if verbose == 1:
                         discrepency = {'ocr_output': discrepency,
                                        'iou_of_bounds': iou_score,
-                                       'fuzz_ratio': fuzz.ratio(t_1.text, t_2.text)}
+                                       'fuzz_ratio': fuzz.ratio(t_1.text,
+                                                                t_2.text)}
 
                     output.append(discrepency)
 
